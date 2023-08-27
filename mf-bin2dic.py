a478924ecb943a0a7c70ece95cf83478d009eb44
SrLPZ <76914115+SrLPZ@users.noreply.github.com>
2023-08-27 14:49:38 +0200

    Update mf-bin2dic.py

--- mf-bin2dic.py
+++ mf-bin2dic.py
@@ -6,7 +6,7 @@ def generate_keys(input_file):
         with open(input_file, 'rb') as bin_file:
             binary_data = bin_file.read()
 
-        keys = [binary_data[i:i+6].hex().upper() for i in range(0, 32 * 6, 6)]
+        keys = [binary_data[i:i+6].hex().upper() for i in range(0, len(binary_data), 6)]
         unique_keys = list(set(keys))
 
         dir_name = os.path.dirname(input_file)
@@ -14,14 +14,16 @@ def generate_keys(input_file):
         output_file = os.path.join(dir_name, f"{file_name}.dic")
 
         with open(output_file, 'w') as keys_file:
-            keys_file.write('\n'.join(unique_keys))
+            for index, key in enumerate(keys):
+                keys_file.write(f"{key}\n")
+
         print(f"Dictionary has been generated and written to {output_file}.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
 # Example usage:
 if len(sys.argv) < 2:
-    print("Usage: python mf-bin2dic.py input_file.bin")
+    print("Usage: python script_name.py input_file.bin")
 else:
     input_file_path = sys.argv[1]
     generate_keys(input_file_path)
